89bfacb57865f105d93b5d640184ecca3e198448
fersamajr <fernando.samaniegojr@gmail.com>
2025-11-12 09:57:15 -0600

    1

--- back_end/src/solicitud-pedidos/solicitud-pedidos.module.ts
+++ back_end/src/solicitud-pedidos/solicitud-pedidos.module.ts
@@ -4,12 +4,14 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { SolicitudPedidosService } from './solicitud-pedidos.service';
 import { SolicitudPedido } from './solicitud-pedido.entity';
 import { UsersModule } from '../users/users.module';
+import { SolicitudPedidosController } from './solicitud-pedido.controller';
 
 @Module({
   imports: [
     TypeOrmModule.forFeature([SolicitudPedido]),
     forwardRef(() => UsersModule), // <--- Importar módulo
   ],
+  controllers: [SolicitudPedidosController],
   providers: [SolicitudPedidosService],
   exports: [SolicitudPedidosService],
 })
