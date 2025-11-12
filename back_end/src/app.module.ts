499428a9abdd370cd08b013a9df4bcb79f6bdeb0
fersamajr <fernando.samaniegojr@gmail.com>
2025-11-11 22:38:58 -0600

    Sql

--- back_end/src/app.module.ts
+++ back_end/src/app.module.ts
@@ -5,15 +5,25 @@ import { UsersModule } from './users/users.module';
 import { PagosModule } from './pagos/pagos.module';
 import { PedidosController } from './pedidos/pedidos.controller';
 import { PedidosModule } from './pedidos/pedidos.module';
-import { SolicitudPedidoController } from './solicitud-pedido/solicitud-pedido.controller';
+import { SolicitudPedidoController } from './solicitud-pedidos/solicitud-pedido.controller';
 import { SolicitudPedidosModule } from './solicitud-pedidos/solicitud-pedidos.module';
 import { PrediccionesModule } from './predicciones/predicciones.module';
 import { LogsController } from './logs/logs.controller';
 import { LogsService } from './logs/logs.service';
 import { LogsModule } from './logs/logs.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [UsersModule, PagosModule, PedidosModule, SolicitudPedidosModule, PrediccionesModule, LogsModule],
+  imports: [UsersModule, PagosModule, PedidosModule, SolicitudPedidosModule, PrediccionesModule, LogsModule, TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'localhost',
+      port: 3306,
+      username: 'root',
+      password: 'Fp$c0105',
+      database: 'nestdb',
+      entities: [__dirname + '/**/*entity{.ts,.js}'],
+      synchronize: true,
+    })],
   controllers: [AppController, PedidosController, SolicitudPedidoController, LogsController],
   providers: [AppService, LogsService],
 })
