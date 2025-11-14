aa20494a420b79d451bdf15aa5269c639da857ec
fersamajr <63007492+fersamajr@users.noreply.github.com>
2025-11-13 22:24:36 -0600

    Agregando front end de prueba con React para consumir el backend.

---
+++ front_end/src/App.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import axios from 'axios';
+
+function App() {
+  const [users, setUsers] = React.useState([]);
+
+  React.useEffect(() => {
+    axios.get('http://localhost:3000/users').then(res => setUsers(res.data));
+  }, []);
+
+  return (
+    <div>
+      <h1>Usuarios</h1>
+      <ul>
+        {users.map(user => (
+          <li key={user.id}>{user.nombre}</li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+export default App;
